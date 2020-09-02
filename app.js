7848d45fa32106f69bb15e1a3b4614676834be90
Lily <apple@apples-mbp.lan>
2020-09-02 17:29:30 -0600

    adding in project from local to github

---
+++ app.js
@@ -0,0 +1,51 @@
+// console.log('Whose that Pokemon?');
+
+$(() => {
+  let x = 0;
+$('form').on('submit', (event) => {
+    event.preventDefault();
+    const userInput = $('input[type="text"]').val();
+    x = userInput;
+    // console.log(x)
+    $.ajax({
+      url: 'https://pokeapi.co/api/v2/pokemon/' + userInput
+      }
+    ).then(
+      (data) => {
+        $('#name').html(data.name)
+        $('#id').html(data.id)
+        $('#height').html(data.height)
+        $('#weight').html(data.weight)
+      })
+  })
+  $("#btn").on('click', (event) => {
+    console.log('button click x: ${x}')
+      x = parseInt(x) + 1
+      if (x >=893){
+        x = parseInt(1)
+      }
+      $.ajax({
+        url: 'https://pokeapi.co/api/v2/pokemon/' + x
+      }).then((data) => {
+        $('#name').html(data.name)
+        $('#id').html(data.id)
+        $('#height').html(data.height)
+        $('#weight').html(data.weight)
+    })
+  })
+  $("#btn2").on('click', (event) => {
+    console.log('button click x: ${x}')
+    x = parseInt(x) - 1
+    if (x <= 0) {
+      x = parseInt(893)
+    }
+      $.ajax({
+        url: 'https://pokeapi.co/api/v2/pokemon/' + x
+      }).then((data) => {
+        $('#name').html(data.name)
+        $('#id').html(data.id)
+        $('#height').html(data.height)
+        $('#weight').html(data.weight)
+        })
+      })
+    })
